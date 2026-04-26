18d30d586c82cc940cdabdeed862f487eebe5666
akyas <b00124354@gmail.com>
2026-04-26 15:09:18 +0700

    fix: correct include paths in proses files

--- api/proses/prosesSurvei.php
+++ api/proses/prosesSurvei.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '../../server/koneksi.php';
+include '../server/koneksi.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') { header("Location: ../survei.php"); exit(); }
 if (!isset($_SESSION['nik'])) { header("Location: ../login.php"); exit(); }
